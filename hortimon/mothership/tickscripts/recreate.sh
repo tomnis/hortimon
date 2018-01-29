282e13216327e89c7c2b1d0186d5e8544bdfeb1e
Tomas McCandless <tomas.mccandless@gmail.com>
2018-01-28 20:49:15 -0800

    template tickscripts and json vars

--- hortimon/mothership/tickscripts/recreate.sh
+++ hortimon/mothership/tickscripts/recreate.sh
@@ -6,8 +6,8 @@ kapacitor define-template generic_mean_alert -tick /usr/local/tickscripts/generi
 kapacitor define clone_temp_alert -template generic_mean_alert -vars /usr/local/tickscripts/clone_temperature.json  -dbrp garden.autogen
 kapacitor define flower_temp_alert -template generic_mean_alert -vars /usr/local/tickscripts/flower_temperature.json  -dbrp garden.autogen
 kapacitor define veg_temp_alert -template generic_mean_alert -vars /usr/local/tickscripts/veg_temperature.json  -dbrp garden.autogen
-kapacitor define clone_humidity_alert -template generic_mean_alert -vars /usr/local/tickscripts/clone_temperature.json  -dbrp garden.autogen
-kapacitor define flower_humidity_alert -template generic_mean_alert -vars /usr/local/tickscripts/flower_temperature.json  -dbrp garden.autogen
+kapacitor define clone_humidity_alert -template generic_mean_alert -vars /usr/local/tickscripts/clone_humidity.json  -dbrp garden.autogen
+kapacitor define flower_humidity_alert -template generic_mean_alert -vars /usr/local/tickscripts/flower_humidity.json  -dbrp garden.autogen
 kapacitor define veg_humidity_alert -template generic_mean_alert -vars /usr/local/tickscripts/veg_humidity.json  -dbrp garden.autogen
 kapacitor enable clone_temp_alert
 kapacitor enable veg_temp_alert
