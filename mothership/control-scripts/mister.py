e3727cbb9aa366e246a51010af5c2345a4b95b23
Tomas McCandless <tomas.mccandless@gmail.com>
2018-04-18 01:21:59 -0700

    remove time option

--- mothership/control-scripts/mister.py
+++ mothership/control-scripts/mister.py
@@ -1,22 +1,36 @@
+from datetime import datetime
+
 from pyHS100 import SmartPlug
 import argparse
+import pytz
 import time
+
+
+def get_sleep_time():
+    """
+    :return: the amount of time (in seconds) that we should sleep for.
+    """
+    hour = datetime.now(pytz.timezone('US/Pacific')).hour
+
+    if hour >= 11 or hour <= 17:
+        return 20
+    else:
+        return 180
 
 
 def main():
     """
     Usage: run with arguments of the time to mist for, and the ip address of the plug
 
-    example:  >> python3 mister.py --time 10 --plug 10.0.1.3
+    example:  >> python3 mister.py --plug 10.0.1.3
     """
     ap = argparse.ArgumentParser()
-    ap.add_argument("-t", "--time", help="duration to mist for (seconds)")
     ap.add_argument("-p", "--plug", help="ip address of the smart plug")
     args = vars(ap.parse_args())
 
-    mist_time = args.get("time")
     plug_ip = args.get("plug")
 
+    mist_time = get_sleep_time()
     print("going to mist for %s" % mist_time)
 
     plug = SmartPlug(plug_ip)
