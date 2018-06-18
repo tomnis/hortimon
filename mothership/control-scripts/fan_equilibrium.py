71c64b518ab8fc8635892c96d17817badd634973
tomas mccandless <tomas.mccandless@gmail.com>
2018-06-17 20:13:29 -0700

    fan script

--- mothership/control-scripts/fan_equilibrium.py
+++ mothership/control-scripts/fan_equilibrium.py
@@ -46,6 +46,7 @@ def main():
     example:  >> python3 fan_equilibrium.py --environment <tag> --series <series> --plug 10.0.1.3
     """
     ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dry-run", help="whether to treat this as a dry run", type=bool, default=False)
     ap.add_argument("-l", "--location", help="zip code or city for weather conditions")
     ap.add_argument("-e", "--environment", help="influxdb tag for the series we are tracking")
     ap.add_argument("-s", "--series", help="influxdb series we are tracking")
@@ -63,16 +64,27 @@ def main():
     indoor_temperature = current_value(series, environment)
     print("%s in %s: %s" % (series, environment, indoor_temperature))
 
-    plug = SmartPlug(plug_ip)
-    print("found plug on ip %s: %s" % (plug_ip, plug.alias))
+    dry_run = args.get("dry_run")
 
     # if its too hot outside, we don't want to bring the hot air in
     if outdoor_temperature > indoor_temperature:
-        print("outdoor temperature is high. turning off fan")
-        #plug.turn_off()
+        print("outdoor temperature is high. fan should be off.")
+	if not dry_run:
+            print("turning off fan...")
+            plug = SmartPlug(plug_ip)
+            print("found plug on ip %s: %s" % (plug_ip, plug.alias))
+            plug.turn_off()
+        else:
+            print("running in dry run mode")
     else:
-        print("turning on fan...")
-        #plug.turn_on()
+        print("outdoor temperature is cool. fan should be on.")
+	if not dry_run:
+            print("turning on fan...")
+            plug = SmartPlug(plug_ip)
+            print("found plug on ip %s: %s" % (plug_ip, plug.alias))
+            plug.turn_on()
+        else:
+            print("running in dry run mode")
 
 
 if __name__ == "__main__":
