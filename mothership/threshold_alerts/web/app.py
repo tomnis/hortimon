64ea44d04ada20a166631de5a8ff795adad21908
tomas mccandless <tomas.mccandless@gmail.com>
2018-06-24 15:19:43 -0700

    extra endpoint for alerts

--- mothership/threshold_alerts/web/app.py
+++ mothership/threshold_alerts/web/app.py
@@ -19,21 +19,43 @@ def hello_world():
     return 'sms-notifier running in docker'
 
 
+def send_sms(message, to_numbers):
+    """
+    Sends given message to the each number in the list.
+    """
+	
+    for number in to_numbers:
+        print("sending sms to " + number)
+        TwilioNotifier.send_sms(number, message)
+
+    return "messages sent: " + str(len(to_numbers))
+    
+
 @app.route('/send-sms', methods=['POST'])
 def send_sms():
     """
-    Sends sms notifications to the comma-separated numbers in the environment var TWILIO_NOTIFY_TO.
+    Sends sms notifications to the comma-separated numbers in the request body field "to_numbers".
     """
     json_event = json.loads(request.data)
     message = json_event["message"]
     to_numbers = json_event["to_numbers"].split(',')
     print("parsed message:" + str(message))
 
-    for number in to_numbers:
-        print("sending sms to " + number)
-        TwilioNotifier.send_sms(number, message)
+    return send_sms(message, to_numbers)
 
-    return "messages sent: " + str(len(to_numbers))
+
+@app.route('/send-sms-to-env', methods=['POST'])
+def send_sms_to_env():
+    """
+    Sends sms notifications to the comma-separated numbers in the environment var TWILIO_NOTIFY_TO.
+    """
+    json_event = json.loads(request.data)
+    message = json_event["message"]
+    to_numbers = os.env["TWILIO_NOTIFY_TO"].split(',')
+    print("parsed message:" + str(message))
+
+    return send_sms(message, to_numbers)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
