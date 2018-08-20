ac367202d4c9e6f138e94792307202170c4cc58e
tomas <tomas.mccandless@gmail.com>
2018-08-20 01:13:13 -0700

    update thresholds (#13)
    * threshold
    
    * threshold
    
    * change default behavior
    
    * threshold update
    
    * threshold update
    
    * air conditioner
    
    * correct import

--- mothership/control-scripts/air_conditioner.py
+++ mothership/control-scripts/air_conditioner.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pyHS100 import SmartPlug
 import argparse
 from influxdb import InfluxDBClient
@@ -5,6 +6,7 @@ from plug_util import find_plug_ip_address, set_plug
 import os
 import requests
 import time
+import pytz
 
 
 def current_value(series, environment):
@@ -56,6 +58,12 @@ def main():
 
     try:
         target_temperature = float(args.get("target_temperature"))
+        hour = datetime.now(pytz.timezone('US/Pacific')).hour
+        # if its "night", we'll add an artificial buffer to increase the target temperature
+        # we don't want it to get too cold ;)
+        if hour > 8 and hour < 21:
+            print("adding buffer zone")
+            target_temperature += 8
         print("target temperature is: %s" % (target_temperature))
             
         environment = args.get("environment")
@@ -72,16 +80,16 @@ def main():
             print("current temperature is low . ac should be off.")
             if not dry_run:
                 state_changed = set_plug(plug_ip, False)
-                if state_changed:
-                    send_notifications("turned off ac (current=%s, target=%s)" % (current_temperature, target_temperature), to_numbers)
+                #if state_changed:
+                #    send_notifications("turned off ac (current=%s, target=%s)" % (current_temperature, target_temperature), to_numbers)
             else:
                 print("running in dry run mode")
         else:
             print("current temperature is warm. ac should be on.")
             if not dry_run:
                 state_changed = set_plug(plug_ip, True)
-                if state_changed:
-                    send_notifications("turned on ac (current=%s, target=%s)" % (current_temperature, target_temperature), to_numbers)
+                #if state_changed:
+                #    send_notifications("turned on ac (current=%s, target=%s)" % (current_temperature, target_temperature), to_numbers)
             else:
                 print("running in dry run mode")
     except Exception as err:
@@ -89,7 +97,13 @@ def main():
         dry_run = args.get("dry_run")
         if not dry_run:
             print("something went wrong, turn on ac")
-            set_plug(plug_ip, True)
+            hour = datetime.now(pytz.timezone('US/Pacific')).hour
+            if hour < 10 or hour >= 20:
+                set_plug(plug_ip, True)
+            else:
+                set_plug(plug_ip, False)
+            
+            send_notification("error %s" % err)
 
 
 if __name__ == "__main__":
