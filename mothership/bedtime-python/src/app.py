fb75cd527c3c1912d03a94a9932c3181abb3db7c
tomas <tomas.mccandless@gmail.com>
2020-03-11 00:11:36 -0700

    bedtime scripts (#18)
    * threshold
    
    * threshold
    
    * change default behavior
    
    * threshold update
    
    * threshold update
    
    * air conditioner
    
    * correct import
    
    * update thresholds
    
    * update thresholds
    
    * retry logic
    
    * minor cleanup
    
    * update thresh
    
    * update threshold
    
    * update threshold
    
    * update threshold
    
    * update threshold
    
    * bedtime scripts
    
    * slow transitions, set temp
    
    * temp update
    
    * docker-compose
    
    * separate compose file
    
    * tweak dimming
    
    * template
    
    * update readme
    
    * mount phu creds
    
    * sliders for input
    
    * vertical sliders
    
    * template vertical
    
    * theme

---
+++ mothership/bedtime-python/src/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from hue_wrapper import HueWrapper
+from background import BackgroundTask
+
+import logging
+import json
+
+logging.basicConfig()
+app = Flask(__name__)
+background = None
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/circle_wave')
+def circle_wave():
+    return render_template('circle_wave.html')
+
+
+@app.route('/bedtime')
+def student():
+   return render_template('bedtime.html')
+
+@app.route('/bedtimePost', methods = ['POST'])
+def bedtime():
+    global background
+
+    if background is not None:
+        print("time in progress, stopping")
+        background.stop()
+
+    result = json.loads(json.dumps(request.form))
+    starting_brightness = int(result[u'brightness'])
+    # total duration of timer in minutes
+    time_minutes = int(result[u'time_minutes'])
+    print("starting new task (%s, %s)" % (starting_brightness, time_minutes))
+    # sleep 60 seconds between transitions
+    hue = HueWrapper("philips-hue.lan")
+    background = BackgroundTask(hue, "tomas lamps", starting_brightness, time_minutes)
+    return 'started bed timer (brightness=%s, time_minutes=%sm)' % (starting_brightness, time_minutes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
