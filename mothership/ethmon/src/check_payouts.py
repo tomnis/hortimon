792e5608bae77a63b436d46320c33f955093c8b6
tomas <tomas.mccandless@gmail.com>
2020-03-28 00:11:56 -0700

    ethmon (#25)
    * threshold
    
    * threshold
    
    * change default behavior
    
    * threshold update
    
    * threshold update
    
    * air conditioner
    
    * correct import
    
    * update thresholds
    
    * update thresholds
    
    * retry logic
    
    * minor cleanup
    
    * update thresh
    
    * update threshold
    
    * update threshold
    
    * update threshold
    
    * update threshold
    
    * bedtime scripts
    
    * slow transitions, set temp
    
    * temp update
    
    * docker-compose
    
    * separate compose file
    
    * tweak dimming
    
    * template
    
    * update readme
    
    * mount phu creds
    
    * sliders for input
    
    * vertical sliders
    
    * template vertical
    
    * theme
    
    * docs
    
    * docs
    
    * traefik routing
    
    * bump traefik version
    
    * overlay
    
    * traefik
    
    * traefik tweaks
    
    * traefik tweaks
    
    * dynamic traefik routers
    
    * remove unused routes
    
    * traefik default middleware
    
    * update network name
    
    * consolidate compose files
    
    * consolidate compose files
    
    * addprefix for chronograf
    
    * restart policies, telegraf
    
    * telegraf conf
    
    * host tag
    
    * comment out kapacitor
    
    * dont use debug
    
    * comment out some telegraf metrics
    
    * sms dockerfile
    
    * minor refactor
    
    * refactor
    
    * exhuast code
    
    * minor rename
    
    * docker
    
    * src dir
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * minor refactor
    
    * docker
    
    * remove unused code
    
    * minor refactor
    
    * minor refactor
    
    * minor refactor
    
    * minor refactor
    
    * minor refactor

---
+++ mothership/ethmon/src/check_payouts.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import os
+import requests
+import schedule
+import time
+
+ethermine_api_url = 'https://api.ethermine.org'
+etherchain_url = 'https://etherchain.org'
+
+# for base unit conversion
+wei_in_ether = 10 ** 18
+
+
+def send_notifications(payout, to_numbers, balance):
+    post_data = {
+            "message" : "eth payout:\n   %.5f\n\nbalance:\n   %.5f" % (payout, balance),
+            "to_numbers" : to_numbers
+            }
+
+    print(post_data)
+    post_url = "http://isengard.lan/sms/send"
+    r = requests.post(post_url, json=post_data)
+    print("POST to notifier server response: " + str(r.status_code))
+    return r
+
+
+
+def get_balance(address_num):
+    url = etherchain_url + '/api/account/' + address_num
+    r = requests.get(url)
+    balance_wei = r.json()['balance']
+    return wei_to_ether(balance_wei)
+
+
+
+def wei_to_ether(wei):
+    return float(wei) / wei_in_ether
+
+
+
+def check_payouts(address, to_numbers):
+    url = ethermine_api_url + '/miner/' + address + '/payouts'
+    r = requests.get(url)
+    # responses are sorted by most recent
+    last_payout = r.json()['data'][0]
+    print(last_payout)
+    payout = wei_to_ether(last_payout['amount'])
+    paid_on = last_payout['paidOn']
+
+    current_time = time.time()
+    # check if the payout was in the last 5 minutes, + some slack)
+    if (current_time - paid_on < (5 * 60 + 8)):
+        address_num = address[2:]
+        send_notifications(payout, to_numbers, get_balance(address_num))
+    else:
+        print("current time is: " + str(current_time) + ", not going to send texts")
+
+def main():
+    address = os.environ["ETHMON_ADDRESS"]
+    print("ethmon_address: " + address)
+    to_numbers = os.environ["ETHMON_TO_NUMBERS"]
+    print("ethmon_to_numbers: " + to_numbers)
+
+    schedule.every(5).minutes.do(lambda: check_payouts(address, to_numbers))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(300)
+
+
+if __name__ == "__main__":
+    main()
+    #check_payouts()
