6b6ce35821d95ea75d22ae1f4703902062382f40
Tomas McCandless <tomas.mccandless@gmail.com>
2020-04-17 01:07:53 -0700

    minor bugfix

--- mothership/barad-dur/src/main.py
+++ mothership/barad-dur/src/main.py
@@ -105,7 +105,7 @@ def scan(camera, capture, hue, strategy):
         # just print that we are likely avoiding a false positive
         elif len(human_rects) > 0:
             print("found humans below threshold {} (likely false positive)".format(human_threshold))
-        elif len(human_rects) == 0 and time.time() - last_seen_human_time > strategy.sleep_when_on:
+        elif len(human_rects) == 0 and time.time() - last_seen_human_time > strategy.sleep_when_on(last_off_time):
             hue.turn_group_off(strategy.hue_group)
             last_off_time = time.time()
 
