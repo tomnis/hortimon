712288c139d073b80243932c4d18c2d1b81a24ca
Tomas McCandless <tomas.mccandless@gmail.com>
2020-04-23 00:49:02 -0700

    mount docker socket into telegraf

--- mothership/barad-dur/src/main.py
+++ mothership/barad-dur/src/main.py
@@ -42,7 +42,7 @@ def get_camera():
     resolution = (640, 480)
     camera = PiCamera()
     camera.resolution = resolution
-    camera.framerate = 2
+    camera.framerate = 1
     camera.contrast = 70
     camera.brightness = 80
     camera.iso = 800
