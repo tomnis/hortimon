eac1074e267dea72cec7ff64464b9efb95dd383c
Tomas McCandless <tomas.mccandless@gmail.com>
2020-04-23 18:56:49 -0700

    minor tweak

--- mothership/barad-dur/src/main.py
+++ mothership/barad-dur/src/main.py
@@ -43,7 +43,7 @@ def get_camera():
     resolution = (640, 480)
     camera = PiCamera()
     camera.resolution = resolution
-    camera.framerate = 2
+    camera.framerate = 3
     camera.contrast = 70
     camera.brightness = 80
     camera.iso = 800
@@ -66,7 +66,7 @@ def scan(camera, capture, hue, strategy):
     :return:
     """
     human_detector = HumanDetector()
-    human_threshold = 0.5
+    human_threshold = 0.6
 
     global last_seen_human_time
     print("scanning video stream...")
@@ -108,7 +108,7 @@ def scan(camera, capture, hue, strategy):
         elif len(human_rects) > 0:
             last_seen_human_time = time.time()
             print("found humans below threshold {} (likely false positive)".format(human_threshold))
-        elif len(human_rects) == 0 and time.time() - last_seen_human_time > 60: # strategy.sleep_when_on(last_off_time):
+        elif len(human_rects) == 0 and time.time() - last_seen_human_time > 90: # strategy.sleep_when_on(last_off_time):
             if group_on is None or group_on is True:
                 print("turning off lights")
                 hue.set_light_group_brightness(strategy.hue_group, brightness)
