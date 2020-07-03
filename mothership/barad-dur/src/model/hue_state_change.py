1d70d51c4fca7664cb227bb85993afdaf7bd2b83
Tomas McCandless <tomas.mccandless@gmail.com>
2020-07-03 16:20:46 -0700

    remove barad-dur

--- mothership/barad-dur/src/model/hue_state_change.py
+++
@@ -1,9 +0,0 @@
-class HueStateChangeEvent:
-
-    def __init__(self, sleep_time):
-        """
-        Represents data returned after a state change event in light status.
-
-        :param sleep_time: amount of time (in seconds) that we should sleep after turning a light on.
-        """
-        self.sleep_time = sleep_time
